8831af3d72315574cc359709da4e39cc38c25d29
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-05 16:03:19 +0900

    Revert "Feat: 테스트, 데이터 조회"
    This reverts commit 06174bb57f8d626c657ca84db8161b9541ad3be0.

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,27 +1,13 @@
 package com.mysite.sbb;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
-    private QuestionRepository questionRepository;
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
 }
