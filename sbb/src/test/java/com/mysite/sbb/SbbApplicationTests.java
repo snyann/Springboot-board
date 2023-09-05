c30e86ce8d3600f35834dbceef4c92a97f04abe0
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-05 16:47:20 +0900

    Feat: 테스트, 답변 데이터 조회
    id 값이 1인 답변을 조회, 그 답변의 질문 id가 2인지도 테스트

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -23,14 +23,9 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a= new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);// 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a=oa.get();
+        assertEquals(2, a.getQuestion().getId());
     }
 }
