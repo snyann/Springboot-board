254c82b21eb7e0479d3c398f4a92f0f9f349f3f5
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-05 16:27:17 +0900

    Feat: 테스트, 데이터 수정
    질문 데이터를 조회한 다음 subject를 "수정된 제목" 이라는 값으로 수정
    변경된 Question 데이터를 저장하기 위해 리포지터리의 save 메서드 사용

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -19,8 +20,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq=this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q=oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
     }
 }
