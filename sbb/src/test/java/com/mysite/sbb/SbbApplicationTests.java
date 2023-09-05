9fe112c59ad605b7ab33abe5fff8955e43737929
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-05 16:13:38 +0900

    Feat: 테스트, 데이터 조회
    findById() 메소드를 통해 qQuestion 엔티티의 Id값으로 데이터 조회

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,6 +2,7 @@ package com.mysite.sbb;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +14,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	void testJpa() {
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
 }
