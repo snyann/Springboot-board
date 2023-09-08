eb6b074f7d71c34dc2af2231be370dc641df5e1d
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-08 18:07:35 +0900

    Feat: 질문 상세 페이지 추가,  DataNotFoundException 클래스
    question_list.html에 질문 상세 링크 추가/ QuestionController에 질문 상세 페이지에 대한 URL 매핑 추가/ QuestionService에 id 값으로 Question 데이터를 조회하는 getQuestion 메서드 추가 / id 값에 해당하는 Question 데이터가 없을 경우에 DataNotFoundException 발생하기 위한 클래스 추가/ URL 프리픽스(prefix) 수정

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,13 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList(){
+    public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        }else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
