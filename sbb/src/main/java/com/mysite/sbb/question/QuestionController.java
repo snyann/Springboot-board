a60882bed0fe0774d47d446a2cc07efafb9f6dfe
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-25 19:44:44 +0900

    Feat: 질문 등록 템플릿 추가
    질문 등록을 위한 question_form.html 템플릿 추가 및 POST 방식으로 요청한 /question/create URL을 처리하기 위해 @PostMapping 애너테이션을 지정한 questionCreate 메서드를 추가

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
@@ -19,15 +22,26 @@ public class QuestionController {
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Question> questionList=this.questionService.getList();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
     @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
-        Question question=this.questionService.getQuestion(id);
+        Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        // TODO 질문을 저장한다.
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
