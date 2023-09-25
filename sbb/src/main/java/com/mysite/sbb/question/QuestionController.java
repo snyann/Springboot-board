3b2d2a37143db0e9e375b517b8baa54b57b87c55
SeungHyeon Yun <hyeondc99@gmail.com>
2023-09-25 20:09:22 +0900

    Feat: 검증에 실패한 오류메시지 추가
    검증에 실패할 경우 오류메시지를 출력할 수 있도록 question_form 템플릿 수정

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -38,7 +38,7 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
